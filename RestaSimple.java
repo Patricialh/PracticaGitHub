d455358039dcce3f064601e6dd39e2c3cc55152c
Patricia Lozano <dnc39975@educastur.es>
2024-12-03 15:15:28 +0100

    Actualizamos archivo de gitignore

---
+++ RestaSimple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class RestaSimple {
+
+	public static void main(String[] args) {
+		char num1, num2;		
+		System.out.printf("Introduce un número");
+			num1 = (char) System.in.read();
+		System.out.printf("Introduce otro número");
+			num2 = (char) System.in.read();
+	}
+
+}
